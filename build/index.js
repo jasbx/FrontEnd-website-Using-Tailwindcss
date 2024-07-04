573ba88a7b02b7b3d057f26f7958650009b4bc99
jassim mohamed <159074105+jasbx@users.noreply.github.com>
2024-07-04 17:05:17 +0300

    Add files via upload

---
+++ build/index.js
@@ -0,0 +1,11 @@
+
+
+const btn =document.getElementById('mobile-open-button')
+const nav =document.getElementById('nav')
+
+
+function ClickedMenuBtn(){
+    nav.classList.toggle('hidden')
+    
+  
+}
